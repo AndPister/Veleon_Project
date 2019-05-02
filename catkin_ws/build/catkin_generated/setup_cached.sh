e99d3472b4152e82315de78d8be9a2cca68cbbc9
AndPister <APister@gmx.de>
2019-05-02 14:54:54 +0100

    first commit

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pi/Veleon_Project/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/pi/Veleon_Project/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pi/Veleon_Project/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/pi/Veleon_Project/catkin_ws/src:$ROS_PACKAGE_PATH"
