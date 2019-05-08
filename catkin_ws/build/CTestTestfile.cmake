404eace2c19c4a5ed406d9fc692fbc7d8adb4c4c
AndPister <apistergmx.de>
2019-05-08 14:01:45 +0200

    including first try launchfile

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andreas/Veleon_Project/catkin_ws/src
-# Build directory: /home/andreas/Veleon_Project/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(core_pkg)
--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/andreas/Veleon_Project/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/andreas/Veleon_Project/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/andreas/Veleon_Project/catkin_ws/devel;/opt/ros/kinetic" != "" else []
