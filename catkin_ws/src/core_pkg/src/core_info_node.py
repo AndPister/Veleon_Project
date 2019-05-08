de3b7558a9020d62e8ea012023107d308ea0b962
AndPister <apister@gmx.de>
2019-05-08 14:42:46 +0200

    first test

--- catkin_ws/src/core_pkg/src/core_info_node.py
+++ catkin_ws/src/core_pkg/src/core_info_node.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 
 def main():
     pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('core_info_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
