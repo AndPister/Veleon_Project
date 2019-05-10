3513132211ad31e0e24b3db2442c7062b509f2ee
AndPister <apister@gmx.de>
2019-05-10 16:41:27 +0200

    publish cpu_usage and enable with param

--- catkin_ws/src/core_pkg/src/core_info_node.py
+++ catkin_ws/src/core_pkg/src/core_info_node.py
@@ -1,33 +1,57 @@
 #!/usr/bin/env python
+
+#####################################################################
+#Project: Veleon-Project
+#Subproject: Publish Coreinformation for performancecheck
+#Autor: Andreas Pister
+#Date:10.05.19
+#####################################################################
+
+
+
 import rospy
 import psutil
 from std_msgs.msg import Float32
 
+enable_param = '/core_info/core_info_enable'
+node_type_name = 'core_info_node'
+core_pub_name = '/core_info/core_usage'
 
-
-def core_info(pub_core,pup_mem,spintime):
+def core_info(pub_core,pub_mem,spintime):
 #Publish core infos
-#CPU-usage RAM-usage
-        rate = rospy.rate(spintime)
+#CPU-usage
+        rate = rospy.Rate(spintime)
         core_usage = psutil.cpu_percent()
-        rospy.loginfo(core_usage)
-        pub.publish(core_usage)
+        memory = psutil.swap_memory()
+        ram_usage = memory
+        rospy.loginfo("Core-usage: " + str(core_usage))
+        pub_core.publish(core_usage)
+        #pub_mem.publish(ram_usage)
+        rate.sleep()
 
-def check_core_release()
-        return True
+def check_core_release():
+        enable = rospy.get_param(enable_param)
+        if enable is None:
+                raise Exception ("No Param : "+enable_param+" found")
+        return enable
 
 def main():
-        pub_core = rospy.Publisher('/core_info_node/core_usage', Float32, queue_size=10)
-        pub_memory= rospy.Publisher('/core_info_node/core_usage', Float32, queue_size=10)
-        rospy.init_node('core_info_node', anonymous=True)
+        pub_core = rospy.Publisher(core_pub_name, Float32, queue_size=10)
+        #pub_memory= rospy.Publisher('/core_info/core_usage', Float32, queue_size=10)
+        pub_memory =None
+        rospy.init_node(node_type_name, anonymous=True)
         while not rospy.is_shutdown():
                 if check_core_release():
-                        core_info(pub_core=pub_core,pub_memory=pub_memory,10)
+                        core_info(pub_core=pub_core,pub_mem=pub_memory,spintime=5)
                 
 
 if __name__=="__main__":
         try:
-                main()
-        except:
-                rospy.loginfo("Error while running core_info_node")
+                if rospy.has_param(enable_param):
+                        main()
+                else:
+                        rospy.loginfo("Param: core_info_enable not found ")
+        except Exception as e:
+                rospy.loginfo("Error while running core_info_node: " + str(e))
+
 
