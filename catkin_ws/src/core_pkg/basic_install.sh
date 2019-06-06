a65b739062530da9175473f1dc83e1841537620c
AndPister <apister@gmx.de>
2019-06-06 13:19:17 +0200

    including Controllernode

--- catkin_ws/src/core_pkg/basic_install.sh
+++ catkin_ws/src/core_pkg/basic_install.sh
@@ -8,13 +8,14 @@
 #Autor:         Andreas Pister
 #Date:          16.05.19
 #Discription:   This Script covers the Main install 
-#               
+#               ROS Configuration
+#               VM-Bugfix 
 #               joy_pkg
 
 
 #Set Configure to .bashrc
 echo "Set Configure to .bashrc"
-_IMPORTSTRING="
+_ROSIMPORT="
 #sourceing workspace\n
 source ~/Veleon_Project/catkin_ws/devel/setup.bash\n
 \n
@@ -22,20 +23,22 @@ source ~/Veleon_Project/catkin_ws/devel/setup.bash\n
 export ROS_MASTER_URI=http://192.168.9.212:11311\n
 #if you use a Virtual-Maschiene to connekt to Bike, you have to setup your IP manuel\n
 export ROS_HOSTNAME=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)\n
-\n
+"
+echo -e $_ROSIMPORT >> ~/.bashrc
+
+echo "Fix VM-Error!"
+_VMIMPORT"
 #error-fix gazebo with VM\n
 export SVGA_VGPU10=0\n
 "
-
-_IP = arp -na | grep -i b8:27:eb
-echo $_IP
-echo -e $_IMPORTSTRING >> ~/.bashrc
-
-
+echo -e $_VMIMPORT >> ~/.bashrc
 
 echo "Install Joy-Package"
 #installing Joy_pkg for Connecting to to xBox-controler
+sudo apt-get install xboxdrv
 sudo apt-get install ros-kinetic-joy
 sudo chmod a+rw /dev/input/js0
+sudo apt install python3-pip -y
+python3 -m pip install -U pygame --user
 
 exit 0
---
+++ catkin_ws/src/core_pkg/src/joy_stick_node.py
@@ -0,0 +1,122 @@
+from pygame
+import rospy
+from geometry_msgs.msg import Twist
+
+node_type_name = "/joy_stick_node"
+pub_twist_name = "/tele_op"
+para_joystic_con_name ="/controller_connected"
+para_autonomus_name = "/autonomDrive"
+para_stop_name = "/stopp"
+para_em_stop_name = "/emergancy_stop"
+
+nr_autonomus_button = 1
+nr_stop_button = 2
+nr_em_stop_button = 3
+nr_rot_axis =1
+nr_trans_axsis = 3
+
+autonomus_button_old# = False
+stop_button_old# = False
+em_stop_button_old# = False
+
+max_translat_speed = 6 #m/s
+max_rot_speed = 6 #m/s
+
+
+def get_twist_info(Joystick):
+    '''
+    Set Controllinput to /tele_op msg
+    Input: -- initialized Joystick-obj 
+    Output: -- Twist-msg
+    '''
+
+    msg = Twist()
+    msg.linear.x = Joystick.pygame.joystick.get_axis(nr_trans_axsis) * max_translat_speed
+    msg.angular.z = Joystick.pygame.joystick.get_axis(nr_rot_axis) * max_rot_speed
+
+    return msg
+
+def set_param(Joystick):
+    '''
+    Set Controllinput to ROS-Param
+    Input: -- initialized Joystick-obj 
+    '''
+
+    if Joystick.get_button(nr_em_stop_button)==1:
+        em_stop_button_old = not em_stop_button_old
+        rospy.set_param(para_em_stop_name, str(em_stop_button_old))
+
+    if Joystick.get_button(nr_autonomus_button)==1:
+        autonomus_button_old = not autonomus_button_old
+        rospy.set_param(para_autonomus_name, str(autonomus_button_old))
+
+    if Joystick.get_button(nr_stop_button)==1:
+        stop_button_old = not stop_button_old
+        rospy.set_param(para_stop_name, str(stop_button_old))
+
+def get_first_param():
+    '''
+    Checks if the parameter are published and set the start value
+    '''
+
+    rospy.loginfo("Initial parameter checkup ")
+
+
+    if rospy.has_param(para_autonomus_name):
+        autonomus_button_old = rospy.get_param(para_autonomus_name) 
+    else:
+        rospy.logwarn(para_autonomus_name + "couldn’t be found: Using default Value")
+        autonomus_button_old = False
+    
+    if rospy.has_param(para_stop_name):
+        stop_button_old = rospy.get_param(para_stop_name)
+    else:
+        rospy.logwarn(para_stop_name + "couldn’t be found: Using default Value")
+        stop_button_old = False
+        
+    if rospy.has_param(para_em_stop_name):
+        em_stop_button_old = rospy.get_param(para_em_stop_name)
+    else:
+        rospy.logwarn(para_em_stop_name + "couldn’t be found: Using default Value")
+        em_stop_button_old = False
+
+def main():
+    '''
+    Main function for Controller integration
+    '''
+
+    rospy.loginfo("initial controllerrequest")
+    #initial controllerrequest
+    pygame.pygame.init()
+    while pygame.joystick.get_init() <=0: #check if controller is available
+        pygame.pygame.quit()
+        pygame.pygame.init()
+
+    Joystick = pygame.joystick.Joystick(0)
+    Joystick.init()
+
+    rospy.set_param(para_joystic_con,"True")
+
+    get_first_param()#geting first paramsetting from ROS-network
+
+    pub_joy = rospy.Publisher(pub_twist_name, Twist, queue_size=10)
+    rospy.init_node(node_type_name, anonymous=True)
+    
+    while not rospy.is_shutdown():
+        data = get_twist_info(Joystick)
+        set_param(Joystick)
+        pub_joy.publish(data)        
+        
+    pygame.quit()
+
+if __name__ == "__main__":
+    try:
+        main()   
+    except Exception as e:
+        rospy.logerr("Error while running joy_stick_node Errorcode:"+str(e))
+
+
+
+
+
+
