aa6bc18082cabb87faaeed2cc33a52a9511884bf
AndPister <APister@gmx.de>
2019-07-09 11:16:50 +0200

    including of The Teleoperation Node and Bugfix

--- catkin_ws/src/core_pkg/src/tele_operation_node.py
+++ catkin_ws/src/core_pkg/src/tele_operation_node.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#########################################################################
+#Project:       Veleon-Project
+#Subproject:    Teleoperation 
+#Autor:         Andreas Pister
+#Date:          09.07.2019
+#
+#Discription:   This Script covers the Teleoperationnode of the 
+#               Veleon-Bike. Also it includes the communication betwen the 
+#               Joy-message and the motioncontole.
+#
+#Input:         /joy-Topic : covers the inpot of the XBox-controler
+#               
+#Output:        /tele_op: the Teleoperation-topic
+#               /autonomDrive-Parameter: indicates the Mode of autonomic driving
+#               /stopp-Parameter: indicates the Mode of stopping the System
+#               /emergancy_stopp: indicates the Mode of fullsoping all procsses
+#
+#########################################################################
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
@@ -28,27 +47,26 @@ default_max_rot_speed = 6 #m/s
 
 class joystickinteraktion():
     
-    def __init__(self,pub_twist_name,button_config,param_config):
-        self.pub_teleop = rospy.Publisher(pub_twist_name, Twist, queue_size=10)
+    def __init__(self,pub_twist_name,sub_joy_name,button_config,param_config):
+        rospy.init_node(node_name,anonymous=False,log_level=rospy.INFO) 
+        rospy.Subscriber(sub_joy_name, Joy, self.callback)
+        self.pub_teleop = rospy.Publisher(pub_twist_name, Twist, queue_size=2)
         self.button_config=button_config
         self.param_config=param_config
         self.button_old=[0 for i in range(0,10)]
         self.default_max_translat_speed = 6 #m/s
         self.default_max_rot_speed = 6 #m/s
+        self.msg=Twist()
 
     def set_twist_info(self,Joystick):
         '''
-        Set Controllinput to /tele_op msg
+        Set ContrSollinput to /tele_op msg
         Input: -- initialized Joystick-obj 
         Output: -- Twist-msg
         '''
-        msg = Twist()
-        msg.linear.x = Joystick[self.button_config["trans_axsis"]]*rospy.get_param(self.param_config['trans_multiply'],self.default_max_translat_speed)
-        msg.angular.z = Joystick[self.button_config["rot_axis"]]*rospy.get_param(self.param_config['rot_multiply'],self.default_max_rot_speed)
-        rospy.logdebug("teleop: %s",msg)
-        self.pub_teleop.publish(msg)
-
-
+        
+        self.msg.linear.x = Joystick[self.button_config["trans_axsis"]]*rospy.get_param(self.param_config['trans_multiply'],self.default_max_translat_speed)
+        self.msg.angular.z = Joystick[self.button_config["rot_axis"]]*rospy.get_param(self.param_config['rot_multiply'],self.default_max_rot_speed)
 
     def set_param_values(self,Buttons):
         '''
@@ -97,36 +115,28 @@ class joystickinteraktion():
             rospy.logwarn("Parameter %s couldn’t be found: Using default Value", self.param_config['em_stop_name'])
             rospy.set_param(self.param_config['em_stop_name'], False)
 
-
-    def calback(self,joy):
+    def callback(self,joy):
         '''
-        Calbackfunktinon for Joy_node topic
-        '''
-        rate = rospy.Rate(5)
+        Callbackfunktinon for Joy_node topic
+        '''  
         self.set_param_values(joy.buttons)
         self.set_twist_info(joy.axes)
-        rate.sleep()
-
-def main():
-    '''
-    Main function for Controller interaktion
-    '''
-    joy_obj =joystickinteraktion(pub_twist_name,button_config,param_config)
-    rospy.init_node(node_name,anonymous=False,log_level=rospy.DEBUG)   
-    joy_obj.get_first_param()                                                       #geting first paramsetting from ROS-network
-    #while not rospy.is_shutdown():
-    rospy.Subscriber(sub_joy_name, Joy, joy_obj.calback)
-    rospy.spin()
-
-
+    
+    def run (self):
+        rate=rospy.Rate(20)
+        while not rospy.is_shutdown():
+            rospy.logdebug(self.msg)
+            self.pub_teleop.publish(self.msg)
+            rate.sleep()    
 
 try:
-    main()   
+    joy_obj =joystickinteraktion(pub_twist_name,sub_joy_name,button_config,param_config)
+    joy_obj.run()  
 except rospy.ROSInterruptException as e:
     rospy.logerr("ROS-Error while runung %s : %s",node_name,str(e))
 
 except Exception as e:
-    rospy.logerr("uncaughed Error while running %s : Errorcode: %s",node_name,str(e))
+    rospy.logerr("Uncaughed error while running %s : Errorcode: %s",node_name,str(e))
 
 
 
