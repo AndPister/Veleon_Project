edfa8b938f8cf9d7dbb9df454aea8f08005ff797
AndPister <apistergmx.de>
2019-07-18 12:09:34 +0200

    Including Rampecontrolle

--- catkin_ws/src/core_pkg/src/motion_controle_node.py
+++ catkin_ws/src/core_pkg/src/motion_controle_node.py
@@ -28,6 +28,7 @@ enable_param = '/motion_controle/simulation_enable'
 node_name = 'motion_controle_node'
 wheel_pub_name = '/motion_controle/wheel_speed'
 service_name ='i2c_sevice'
+spinningrate= 10
 
 adress_motor_left = 0x2a
 adress_motor_right =0x3a
@@ -35,9 +36,38 @@ x_dot_default = 0.0
 alpha_dot_default = 0.0
 accuracy= 3
 
-
+beschleunigungsrampe = 0.5
+verzögerungsrampe = 0.5
+phi_dot_alt= [0.000,0.000] # [links,rechts]
 R = 0.503         
 d = 0.77/2
+class generate_ramp():  
+    def generate_ramp(self,phi_dot,phi_dot_alt):
+        self.phi_dot =phi_dot
+        self.phi_dot_alt = phi_dot_alt
+        if self.phi_dot = phi_dot_alt:
+            return self.phi_dot
+        elif self.phi_dot < 0 and self.phi_dot_alt < 0:
+            self.phi_dot*= -1
+            self.phi_dot_alt*= -1
+            return beschleunigung()*-1
+        elif self.phi_dot> self.phi_dot_alt:
+            return beschleunigung() 
+        elif self.phi_dot < self.phi_dot_alt:
+            return verzögerung()
+
+    def beschleunigung(self,sign):
+        phi_dot_output = self.phi_dot_alt + beschleunigungsrampe*(1/spinningrate)
+        if self.phi_dot < phi_dot_output:
+            phi_dot_output = self.phi_dot
+        return phi_dot_output
+
+    def verzögerung(self):
+        phi_dot_output = self.phi_dot_alt - verzögerungsrampe*(1/spinningrate)
+        if self.phi_dot < phi_dot_output:
+            phi_dot_output = self.phi_dot
+        return phi_dot_output 
+
 
 def kinematics(cmd_vel):
     '''
@@ -47,15 +77,23 @@ def kinematics(cmd_vel):
     '''
     x_dot = cmd_vel.linear.x
     alpha_dot = cmd_vel.angular.z 
-    
+    rampe= generate_ramp()
+
     #check if join-angle is in Tolerance
     if rospy.get_param('/emergancy_stopp',False): 
         phi_dot_r = (x_dot+d*alpha_dot)/R
         phi_dot_l = (x_dot-d*alpha_dot)/R
+        phi_dot_r = rampe.generate_ramp(phi_dot_r,phi_dot_alt[1])
+        phi_dot_l = rampe.generate_ramp(phi_dot_l,phi_dot_alt[0])
+        phi_dot_alt=[phi_dot_l,phi_dot_r]
     elif rospy.get_param('/emergancy_stopp'):
         rospy.logerr("Emergency stop is active ")
         phi_dot_r = 'F'
         phi_dot_l = 'F'
+    elif rospy.get_param('/stopp',False):
+        phi_dot_r = rampe.generate_ramp(0.0,phi_dot_alt[1])
+        phi_dot_l = rampe.generate_ramp(0.0,phi_dot_alt[0])
+        phi_dot_alt=[phi_dot_l,phi_dot_r]
     else:
         rospy.logerr("Circle to small !! Default Values are used!! x_dot: %s alpha_dot:%s",x_dot_default,alpha_dot_default)
         phi_dot_r = 0.0
@@ -66,9 +104,11 @@ def listener():
     rospy.init_node(node_name,anonymous=False)
     topic_name ='/tele_op'
     rospy.logdebug("Used Topic: %s", topic_name)
+    rate = rospy.Rate(spinningrate)
     while not rospy.is_shutdown():
         topic_name = check_topic(topic_name)
         rospy.Subscriber(topic_name, Twist, calback)
+        rate.sleep()
 
 def send_value(phi_dot,address):
     rospy.wait_for_service(service_name)
